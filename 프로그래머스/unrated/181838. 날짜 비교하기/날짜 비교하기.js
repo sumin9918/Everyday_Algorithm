6c9099d1a6398bd4bb78cbb12387fd770ad12a0c
최수민 <104312167+sumin9918@users.noreply.github.com>
2023-06-11 22:28:43 +0900

    [unrated] Title: 날짜 비교하기, Time: 0.13 ms, Memory: 33.9 MB -BaekjoonHub

--- 프로그래머스/unrated/181838. 날짜 비교하기/날짜 비교하기.js
+++ 프로그래머스/unrated/181838. 날짜 비교하기/날짜 비교하기.js
@@ -1,6 +1,3 @@
 function solution(date1, date2) {
-    const [year1, month1, day1] = date1;
-    const [year2, month2, day2] = date2;
-    
-    return +(new Date(year1, month1, day1) < new Date(year2, month2, day2))
+    return +(new Date(...date1) < new Date(...date2))
 }
