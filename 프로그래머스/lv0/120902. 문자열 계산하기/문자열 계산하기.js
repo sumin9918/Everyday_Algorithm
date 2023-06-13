9bc0dd53a386241773f7176e27fbecdd2f6e1b6a
최수민 <104312167+sumin9918@users.noreply.github.com>
2023-06-13 23:28:52 +0900

    [level 0] Title: 문자열 계산하기, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120902. 문자열 계산하기/문자열 계산하기.js
+++ 프로그래머스/lv0/120902. 문자열 계산하기/문자열 계산하기.js
@@ -1,3 +1,3 @@
 function solution(my_string) {
-    return eval(my_string)
+    return my_string.split(' ').reduce((acc, cur, i, arr) => i % 2 ? (cur === '+' ? acc + +arr[i+1] : acc - arr[i+1]) : acc, +my_string.split(' ')[0]);
 }
