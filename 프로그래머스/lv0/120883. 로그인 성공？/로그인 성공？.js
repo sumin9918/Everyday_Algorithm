5f9a4221bf4030d05ea78f94fa7fd0ee703702b8
최수민 <104312167+sumin9918@users.noreply.github.com>
2023-06-15 17:45:13 +0900

    [level 0] Title: 로그인 성공?, Time: 0.07 ms, Memory: 33.3 MB -BaekjoonHub

--- 프로그래머스/lv0/120883. 로그인 성공？/로그인 성공？.js
+++ 프로그래머스/lv0/120883. 로그인 성공？/로그인 성공？.js
@@ -1,5 +1,5 @@
 function solution(id_pw, db) {
     const filteredDb = db.filter(data => data[0] === id_pw[0]).flat();
     
-    return filteredDb.length === 0 ? 'fail' : filteredDb[1] === id_pw[1] ? 'login' : 'wrong pw';
+    return filteredDb.length ? filteredDb[1] === id_pw[1] ? 'login' : 'wrong pw' : 'fail';
 }
