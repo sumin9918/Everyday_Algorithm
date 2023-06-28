01c1792c97e8b8ad1446450beb807d505f0d8d44
최수민 <104312167+sumin9918@users.noreply.github.com>
2023-06-28 23:06:20 +0900

    [Bronze V] Title: A-B, Time: 128 ms, Memory: 9340 KB -BaekjoonHub

---
+++ 백준/Bronze/1001. A－B/A－B.js
@@ -0,0 +1,9 @@
+const fs = require("fs");
+const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
+const input = fs.readFileSync(filePath).toString().trim().split("\n");
+
+for (let i = 0; i < input.length; i++) {
+  const [A, B] = input[i].split(" ");
+
+  console.log(A - B.replace("\r", ""));
+}
