d87df501710cbab1ee693bf4c189c538c6ee68ae
최수민 <104312167+sumin9918@users.noreply.github.com>
2023-06-28 23:44:06 +0900

    [Bronze V] Title: 나머지, Time: 124 ms, Memory: 9592 KB -BaekjoonHub

---
+++ 백준/Bronze/10430. 나머지/나머지.js
@@ -0,0 +1,15 @@
+const fs = require("fs");
+const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
+const input = fs.readFileSync(filePath).toString().trim().split("\n");
+
+for (let i = 0; i < input.length; i++) {
+  const [a, b, c] = input[i].split(" ");
+  const A = +a;
+  const B = +b;
+  const C = +c;
+
+  console.log((A + B) % C);
+  console.log(((A % C) + (B % C)) % C);
+  console.log((A * B) % C);
+  console.log(((A % C) * (B % C)) % C);
+}
