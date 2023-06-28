25cadf59fbd4c828d595ee86330d883580b6dd8b
최수민 <104312167+sumin9918@users.noreply.github.com>
2023-06-28 23:30:44 +0900

    [Bronze V] Title: 1998년생인 내가 태국에서는 2541년생?!, Time: 132 ms, Memory: 9584 KB -BaekjoonHub

---
+++ 백준/Bronze/18108. 1998년생인 내가 태국에서는 2541년생？！/1998년생인 내가 태국에서는 2541년생？！.js
@@ -0,0 +1,7 @@
+const fs = require("fs");
+const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
+const input = fs.readFileSync(filePath).toString().trim().split("\n");
+
+for (let i = 0; i < input.length; i++) {
+  console.log(input[i] - 543);
+}
