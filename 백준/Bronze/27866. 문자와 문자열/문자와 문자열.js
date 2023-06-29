a2c695cbdd311eb39ffe3791fe8990a2d46d52da
최수민 <104312167+sumin9918@users.noreply.github.com>
2023-06-30 01:04:21 +0900

    [Bronze V] Title: 문자와 문자열, Time: 140 ms, Memory: 9328 KB -BaekjoonHub

---
+++ 백준/Bronze/27866. 문자와 문자열/문자와 문자열.js
@@ -0,0 +1,5 @@
+const fs = require("fs");
+const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
+const input = fs.readFileSync(filePath).toString().trim().split("\n");
+
+console.log(input[0].charAt(input[1] - 1));
