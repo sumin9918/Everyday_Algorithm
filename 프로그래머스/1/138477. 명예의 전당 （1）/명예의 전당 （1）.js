d9b20aac8dfcca77ef862baf94df40c44481f118
최수민 <104312167+sumin9918@users.noreply.github.com>
2023-10-14 21:01:05 +0900

    [level 1] Title: 명예의 전당 (1), Time: 0.08 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/138477. 명예의 전당 （1）/명예의 전당 （1）.js
+++ 프로그래머스/1/138477. 명예의 전당 （1）/명예의 전당 （1）.js
@@ -1,15 +1,12 @@
 function solution(k, score) {
-    const answer = [];
     let winner = [];
     
-    for (let i = 0; i < score.length; i++) {
+    return score.map((s, i) => {
         if (winner.length < k) winner = [...winner, score[i]];
         else if (winner[0] < score[i]) winner[0] = score[i];
         
         winner.sort((a, b) => a - b);
         
-        answer.push(winner[0]);
-    };
-    
-    return answer;
+        return winner[0];
+    });
 }
