2c3f6de15a6abb511fa7c86608b465a978dcb726
최수민 <104312167+sumin9918@users.noreply.github.com>
2023-10-16 23:31:23 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 0.11 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.js
+++ 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.js
@@ -1,22 +1,12 @@
-const nums = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
+const numbers = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"];
 
 function solution(s) {
-    let answer = '';
-    let chars = '';
-    
-    for (let i = 0; i < s.length; i++) {
-        if (!isNaN(s[i])) {
-            answer += s[i];
-            continue;
-        }
-        
-        chars += s[i];
-        
-        if (nums.indexOf(chars) !== -1) {
-            answer += nums.indexOf(chars);
-            chars = '';
-        }
+    let answer = s;
+
+    for(let i=0; i< numbers.length; i++) {
+        const arr = answer.split(numbers[i]);
+        answer = arr.join(i);
     }
-    
+
     return +answer;
 }
